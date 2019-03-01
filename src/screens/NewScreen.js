22031b86f0b1afa030fce5830f70bd06aab8f77a
Pedro Henrique <phgp25@gmail.com>
2019-03-01 18:15:16 -0300

    screen new done

--- src/screens/NewScreen.js
+++ src/screens/NewScreen.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import {StyleSheet, View } from 'react-native'
 import Myheader from '../components/Header';
+import { Input, Button } from 'react-native-elements';
 
 export default class NewScreen extends Component {
   render() {
     return (
       <View>
         <Myheader navigation={this.props.navigation} title='Novo Produto'></Myheader>
+        <Input placeholder='Nome'></Input>
+        <Input placeholder='Temp. Idemal'></Input>
+        <Button buttonStyle={styles.button} title='Salvar'></Button>
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  button:{
+    margin:10
+  }
+})
